bd7b4f53a392feb747e507a8468540dd5f2f5822
magicianlib <magicianlib@gmail.com>
2026-08-09 14:29:05 +0800

    response

---
+++ cookbook-payload/src/main/java/io/ituknown/payload/AbstractResponse.java
@@ -0,0 +1,9 @@
+package io.ituknown.payload;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public abstract class AbstractResponse implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 710256058654406926L;
+}
